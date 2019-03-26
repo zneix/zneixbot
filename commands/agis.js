4e786267d8cc652b35cc81f3f45c8361765bc293
Paweł <44851575+zneix@users.noreply.github.com>
2019-03-26 11:18:12 +0100

    v0.7.1 released

--- commands/agis.js
+++ commands/agis.js
@@ -2,23 +2,23 @@ module.exports = {
     name: `agis`,
     description: `**plays aguz\'s favouite song**`,
     execute(message, bot) {
-		if (message.member.voiceChannel) {
-			message.member.voiceChannel.join()
-			.then(connection => {
+			if (message.member.voiceChannel) {
+				message.member.voiceChannel.join()
+				.then(connection => {
 				const dispatcher = connection.playFile('./media/agis.mp3');
 				message.channel.send('Playing agis!')
 				.then(() => console.log('playing agis!'));
 				dispatcher.on('end', () => {
-					// The song has finished
-					message.member.voiceChannel.join()
+				// The song has finished
+				message.member.voiceChannel.join()
 					.then(connection => {
 						message.member.voiceChannel.leave();
-						message.channel.send(`I've disconnected from ${bot.user.voiceChannel}!`);
+						message.channel.send(`I've disconnected from ${message.member.voiceChannel}!`);
 					})
 				});
-			}).catch(console.error);
+				}).catch(console.error);
 		} else {
 			message.channel.send(`Join voice channel first **( ͡° ͜ʖ ͡°)**`);
 			}
     },
-}
+};
--- commands/devtool.js
+++ commands/devtool.js
@@ -3,17 +3,31 @@ module.exports = {
     name: "devtool",
     description: "This is a developer tool, do not use it",
     async execute(message, args, bot, config, smark, bdayboi) {
-        if (message.author.id != config.devid) return message.channel.send(`This is a developer tool, you're not allowed to use it!`);
+        // if (message.author.id != config.devid) return message.channel.send(`This is a developer tool, you're not allowed to use it!`);
         if (!args.length) return message.reply(`You're epic, no args yikes!`);
         if (args[0] === `msg`) {
             bot.channels.get(args[1]).send(args[2]);
             return null;
         }
         if (args[0] === `channels` || args[0] === `chs`) {
-            frame.push(`**Channels of '${bot.guilds.get(args[1]).name}' (${bot.guilds.get(args[1]).channels.size}):**\n`)
+            frame.push(`**Channels of '${bot.guilds.get(args[1]).name}' (${bot.guilds.get(args[1]).channels.size}):**\n`);
             bot.guilds.get(args[1]).channels.forEach(ch => {
                 frame.push(`[${ch.type}] '${ch.name}' (${ch.id})`);
             });
+            await message.channel.send(frame);
+            frame.length = 0;
+            return null;
+        }
+        if (args[0] === `guilds` || args[0] === `servers`) {
+            frame.push(`**All user guilds mapped by their ID:**`);
+            bot.guilds.forEach(g => {frame.push(`${g.id} '__${g.name}__'`);});
+            await message.channel.send(frame);
+            frame.length = 0;
+            return null;
+        }
+        if (args[0] === `users`) {
+            frame.push(`**All users in my guilds:**`);
+            bot.users.forEach(u => {frame.push(`${u.id} '__${u.tag}__'`);});
             await message.channel.send(frame);
             frame.length = 0;
             return null;
