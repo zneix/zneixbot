ed8c3b4f1a1edae66b304987e641ca13e142ad7f
Paweł <44851575+zneix@users.noreply.github.com>
2019-04-11 22:33:20 +0200

    another hotfix Pepega

--- bot.js
+++ bot.js
@@ -4,8 +4,8 @@ const bot = new discord.Client();
 bot.login(process.env.token);
 const config = require('./config.json');
 // const config = require('./config-beta.json');
-bot.login(config.tokenBETA);
-// bot.login(config.token);
+// bot.login(config.tokenBETA);
+bot.login(config.token);
 // ======================================================== CHANGE THE TOKEN ========================================================
 const database = require(config.dbpath);
 bot.commands = new discord.Collection();
