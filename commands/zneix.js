512ac0f18998d42cbf01192479ecdf674a93be4f
Paweł <44851575+zneix@users.noreply.github.com>
2019-04-11 22:25:41 +0200

    yEET

--- commands/zneix.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: `zneix`,
-    description: `my dev OwO`,
-    execute(message) {
-        message.channel.send(`message from dev: \`\`\`050419 or 080419\nalso, gonna be a KD mod/admin soon\`\`\``);
-    },
-};
