7a864b099caf76cf22826cff57c13a8f875b6326
Paweł <zzneix@gmail.com>
2019-07-04 12:43:36 +0200

    expanded error handler, so it works now

--- extensions/errorHandler.js
+++ extensions/errorHandler.js
@@ -3,27 +3,37 @@ Discord.Message.prototype.command = async function(num, func){
     try {
         var args = this.content.split(/ +/g);
         if (num) {
-            // if (num + 1 <= args.length) throw "Too many arguments!" //I guess this one is useless for now, so I've disabled it
-            if (num - 1 >= args.length) throw "Too few arguments!"
+            // if (num <= args.length) throw "Too many arguments!" //I guess this one is useless for now, so I've disabled it
+            if (num >= args.length) throw "Too few arguments!"
         }
         func().catch(async err => {
             console.log(err);
             console.trace("Async/Promise rejection command error: "+err);
-            embedError();
+            var embed = {
+                color: 0xff5050,
+                author: {
+                        name:this.guild.name+" — \""+this.channel.name+"\"",
+                        icon_url: this.author.avatarURL
+                    },
+                    description: "**"+this.author.username+"#"+this.author.discriminator+":"+this.author.id+"** failed to call: ***"+this.content+"***",
+                    fields:[
+                        {
+                            name: "Reason:",
+                            value: err.substring(0,1023),
+                        }
+                    ],
+                    timestamp: new Date()
+            }
+            this.channel.send({embed:embed}).then(msg => {if (this.client.config.delete.error) msg.delete(this.client.config.delete.time)});
             
         });
     }
     catch (error) {
         console.trace("Sync command error: "+error);
-        embedError();
-        
-    }
-    this.delete(0);
-    embedError = async function(){
         var embed = {
             color: 0xff5050,
             author: {
-                    name:this.channel.guild.name+" — \""+this.channel.name+"\"",
+                    name:this.guild.name+" — \""+this.channel.name+"\"",
                     icon_url: this.author.avatarURL
                 },
                 description: "**"+this.author.username+"#"+this.author.discriminator+":"+this.author.id+"** failed to call: ***"+this.content+"***",
@@ -37,4 +47,5 @@ Discord.Message.prototype.command = async function(num, func){
         }
         this.channel.send({embed:embed}).then(msg => {if (this.client.config.delete.error) msg.delete(this.client.config.delete.time)});
     }
+    this.delete(0);
 }
