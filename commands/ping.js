88501a47e04d7305befa300ae634bb09dcade339
Paweł <zzneix@gmail.com>
2019-07-04 12:04:51 +0200

    changed exports.name format

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,4 @@
-exports.name = `**{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}**`;
+exports.name = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`;
 exports.description = `Pings a bot to check if it's online.`;
 exports.usage = `**{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}**`
 exports.perms = `user`
