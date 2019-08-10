aedc5de315abe1ab043d09d8899d0543328afdc1
Paweł <zzneix@gmail.com>
2019-08-10 14:03:47 +0200

    added server.js command

---
+++ commands/server.js
@@ -0,0 +1,58 @@
+exports.name = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`;
+exports.description = `Displays various information about current server.`;
+exports.usage = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`
+exports.perms = `user`
+
+exports.run = async (client, message) => {
+    message.command(false, async () => {
+        let time = require('../utils/timeUtil');
+        var embed = {
+            color: 0xcc44ff,
+            timestamp: new Date(),
+            footer: {
+                text: message.author.tag,
+                icon_url: message.author.avatarURL
+            },
+            author: {
+                name: message.guild.name,
+                icon_url: message.guild.iconURL
+            },
+            fields: [
+                {
+                    name: "Time created",
+                    value: time.dateFormat(message.guild.createdAt)+`\n\`${time.msFormat(Date.now()-message.guild.createdTimestamp)} ago\``,
+                    inline: true
+                },
+                {
+                    name: "ID",
+                    value: "Server: "+message.guild.id+"\nOwner: "+message.guild.owner.id,
+                    inline: true
+                },
+                {
+                    name: "Owner",
+                    value: message.guild.owner.user+" "+message.guild.owner.user.tag,
+                    inline: false
+                },
+                {
+                    name: "Members",
+                    value: message.guild.members.filter(m => m.user.presence.status !== "offline").size+"/"+message.guild.members.size+` (${message.guild.members.filter(m => m.user.bot).size} bots)`,
+                    inline: true
+                },
+                {
+                    name: "Roles",
+                    value: message.guild.roles.size,
+                    inline: true
+                },
+                {
+                    name: "Channels",
+                    value: `Total: **${message.guild.channels.size}**`
+                    +`\nCategories: **${message.guild.channels.filter(ch => ch.type === "category").size}**`
+                    +`\nText: **${message.guild.channels.filter(ch => ch.type === "text").size}**`
+                    +`\nVoice: **${message.guild.channels.filter(ch => ch.type === "voice").size}**`,
+                    inline: false
+                }
+            ]
+        }
+        message.channel.send({embed:embed});
+    });
+}
