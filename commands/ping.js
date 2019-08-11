51a3a37ac498c5f50d620c075de77372c0e8467f
Paweł <zzneix@gmail.com>
2019-08-11 02:32:10 +0200

    united usage format

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 exports.name = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`;
 exports.description = `Pings a bot to check if it's online.`;
-exports.usage = `**{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}**`
+exports.usage = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`
 exports.perms = `user`
 
 exports.run = async (client, message) => {
--- commands/reload.js
+++ commands/reload.js
@@ -1,6 +1,6 @@
 exports.name = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`;
 exports.description = `Reloads command with latest code.`;
-exports.usage = `**{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}** ***(command)***`
+exports.usage = `**{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}** **(command)**`
 exports.perms = `owner`
 
 exports.run = async (client, message) => {
