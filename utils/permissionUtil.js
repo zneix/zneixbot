36af1e16fb43b46c55d25bc43004a79a46273bb6
Paweł <zzneix@gmail.com>
2019-08-11 06:27:22 +0200

    added perms handler util

---
+++ utils/permissionUtil.js
@@ -0,0 +1,30 @@
+module.exports = (client, message) => {
+    let perms = client.perms;
+    let id = message.author.id;
+
+    let owner = function(){
+        if (!perms.owner.includes(id)) return false;
+        return true;
+    }
+    let admin = function(){
+        if (!perms.owner.includes(id) && !perms.admin.includes(id)) throw "This command requires **bot administrator** prvileges to run!"
+        return console.log("admin command called!");
+    }
+    let mod = function(){
+        if (!perms.owner.includes(id) && !perms.admin.includes(id) && !perms.mod.includes(id)) throw "This command requires **bot moderator** prvileges to run!"
+        return console.log("mod command called!");
+    }
+    let banned = function(){
+        if (perms.ban.includes(id)) throw "You are banned from the bot!"
+    }
+    let check = function(perms){
+        if (!message.member.hasPermission(perms)) throw `This command requires ${perms.length === 1?`**${perms}** permission`:`**${perms.join(", ")}** permissions`} to run!`
+    }
+    return {
+        check: check,
+        isOwner: owner,
+        isAdmin: admin,
+        isMod: mod,
+        isBanned: banned
+    }
+}
