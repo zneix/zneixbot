c4ef103eb94bc4da9fd89917e79263fc977715c1
Paweł <zzneix@gmail.com>
2019-08-11 02:12:26 +0200

    added user.js command

---
+++ commands/user.js
@@ -0,0 +1,64 @@
+exports.name = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`;
+exports.description = `Displays various information about user.`;
+exports.usage = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)} [user ID | @mention]`
+exports.perms = `user`
+
+exports.run = async (client, message) => {
+    message.command(false, async () => {
+        let time = require('../utils/timeUtil');
+        let result = function(user){
+            let member = message.guild.members.get(user.id);
+            let arr = [];
+            member?member.roles.forEach(r => {if (r.id !== message.guild.id) arr.push(r.toString())}):null //roles thingy
+            var embed = {
+                color: member?member.displayColor:0x000000,
+                timestamp: new Date(),
+                footer: {
+                    text: message.author.tag,
+                    icon_url: message.author.avatarURL
+                },
+                author: {
+                    name: user.tag,
+                    icon_url: user.avatarURL
+                },
+                thumbnail: user.avatarURL,
+                description: user.toString(),
+                fields: [
+                    {
+                        name: "User ID",
+                        value: user.id,
+                        inline: true
+                    },
+                    {
+                        name: "Status",
+                        value: user.presence.status==="offline"?"offline":user.presence.status==="online"?"online":`online (${user.presence.status})`
+                    },
+                    {
+                        name: "Created at",
+                        value: time.dateFormat(user.createdAt)+` \`${time.msFormat(Date.now()-user.createdTimestamp)} ago\``,
+                        inline: true
+                    },
+                    {
+                        name: "Joined at",
+                        value: member?time.dateFormat(member.joinedAt)+` \`${time.msFormat(Date.now()-member.joinedTimestamp)} ago\``:"The user is not in this guild.",
+                        inline: true
+                    },
+                    {
+                        name: `Roles${member?` [${arr.length}]`:""}`,
+                        value: member?arr.length?arr.join(" "):"None.":"The user is not in this guild.",
+                        inline: false
+                    }
+                ]
+            }
+            return message.channel.send({embed:embed});
+        }
+        if (!message.args.length) return result(message.author);
+        let taggedUser = message.mentions.users.first();
+        if (!taggedUser) {
+            let validUser = client.users.get(message.args[0]);
+            if (validUser) return result(validUser);
+            else return result(message.author);
+        }
+        else return result(taggedUser);
+    });
+}
