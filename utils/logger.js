b17825b7d3517e2f86429b24d5c3ecf1b9a4734b
Paweł <zzneix@gmail.com>
2019-08-12 15:29:55 +0200

    created logger for event 'ready'

--- utils/logger.js
+++ utils/logger.js
@@ -1,4 +1,35 @@
 module.exports = client => {
+    let ready = function(client){
+        console.log(`[ready] Connected as: '${client.user.tag}'`);
+        let logs = client.channels.get(client.config.channels.logs);
+        if (logs) {
+            var embed = {
+                color: 0xf97304,
+                timestamp: new Date(),
+                footer: {
+                    text: client.user.tag,
+                    icon_url: client.user.avatarURL
+                },
+                author: {
+                    name: "Logged in to WebSocket"
+                },
+                fields: [
+                    {
+                        name: "User",
+                        value: client.user+"\n"+client.user.tag+" `"+client.user.id+"`",
+                        inline: false
+                    },
+                    {
+                        name: "Size",
+                        value: `Users: **${client.users.size}**\nGuilds: **${client.guilds.size}**\nChannels: **${client.channels.size}**\nEmotes: **${client.emojis.size}**`,
+                        inline: false
+                    }
+                ]
+            }
+            logs.send({embed:embed});
+        }
+        else console.log(`[!ready] logs channel not found!`); //code executed as an error message
+    }
     let command = function(message, cmd, level){
         console.log(`(cmd; level ${level}) ${cmd.name.replace(/{PREFIX}/, "")}`);
         let logs = client.channels.get(client.config.channels.logs);
@@ -20,6 +51,7 @@ module.exports = client => {
         else console.log(`(!cmd) logs channel not found!`);
     }
     return {
+        ready: ready,
         command: command
     }
 }
