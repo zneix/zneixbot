8414a0860cf04b8c15b4caa2ffd5ab9a193c10c6
Paweł <zzneix@gmail.com>
2019-08-12 15:30:39 +0200

    used external logger

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,4 @@
 module.exports = client => {
-    console.log(`[ready] Connected as: '${client.user.tag}'`);
     client.user.setPresence({
         status: 'dnd',
         game: {
@@ -9,30 +8,6 @@ module.exports = client => {
             type: 1
         }
     });
-    var embed = {
-        color: 0xf97304,
-        timestamp: new Date(),
-        footer: {
-            text: client.user.tag,
-            icon_url: client.user.avatarURL
-        },
-        author: {
-            name: "Logged in to WebSocket"
-        },
-        fields: [
-            {
-                name: "User",
-                value: client.user+"\n"+client.user.tag+" `"+client.user.id+"`",
-                inline: false
-            },
-            {
-                name: "Size",
-                value: `Users: **${client.users.size}**\nGuilds: **${client.guilds.size}**\nChannels: **${client.channels.size}**\nEmotes: **${client.emojis.size}**`,
-                inline: false
-            }
-        ]
-    }
-    let logs = client.channels.get(client.config.channels.logs);
-    if (logs) logs.send({embed:embed});
-    else console.log(`[!ready] logs channel not found!`); //code executed as an error message
+    const logger = require('../utils/logger')(client);
+    logger.ready(client);
 }
