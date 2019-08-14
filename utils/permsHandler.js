6dc5fa2c0a35f2e233618dacb4a894d4c009942a
Paweł <zzneix@gmail.com>
2019-08-15 01:15:37 +0200

    added permission level check

--- utils/permsHandler.js
+++ utils/permsHandler.js
@@ -15,11 +15,18 @@ module.exports = (client, message) => {
     let banned = function(){
         if (perms.ban.includes(id)) throw "You are banned from the bot!"
     }
-    let check = function(perms){
+    let guildperm = function(perms){
         if (!message.member.hasPermission(perms)) throw `This command requires ${perms.length === 1?`**${perms}** permission`:`**${perms.join(", ")}** permissions`} to run!`
     }
+    let levelCheck = function(){
+        if (perms.owner.includes(id)) return "owner";
+        if (perms.admin.includes(id)) return "admin";
+        if (perms.mod.includes(id)) return "mod";
+        return "user";
+    }
     return {
-        check: check,
+        guildperm: guildperm,
+        levelCheck: levelCheck,
         isOwner: owner,
         isAdmin: admin,
         isMod: mod,
