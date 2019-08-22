ade30308432cda697b8f63fb37734260e7d3fdd8
Paweł <zzneix@gmail.com>
2019-08-22 19:53:04 +0200

    added lookup by name and emote ID

--- utils/emoteHandler.js
+++ utils/emoteHandler.js
@@ -1,4 +1,12 @@
 module.exports = client => {
+    function find(emoteName){
+        let emote = client.emojis.find(e => e.name === emoteName);
+        return emote?emote:null;
+    }
+    function id(emoteID){
+        let emote = client.emojis.get(emoteID);
+        return emote?emote:null;
+    }
     function dev(emoteName){
         let emote = client.guilds.get("288029287892910100").emojis.find(e => e.name === emoteName);
         return emote?emote:null;
@@ -8,6 +16,8 @@ module.exports = client => {
         return emote?emote:null;
     }
     return {
+        find: find,
+        id: id,
         dev: dev,
         asset: asset
     }
