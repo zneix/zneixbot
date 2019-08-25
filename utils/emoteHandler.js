af3b90522f3126248adb917068cb872d25e145e2
Paweł <zzneix@gmail.com>
2019-08-25 14:58:41 +0200

    updated rejected promise returns

--- utils/emoteHandler.js
+++ utils/emoteHandler.js
@@ -1,19 +1,19 @@
 module.exports = client => {
     function find(emoteName){
         let emote = client.emojis.find(e => e.name === emoteName);
-        return emote?emote:emoteName;
+        return emote?emote:"`:"+emoteName+":`";
     }
     function id(emoteID){
         let emote = client.emojis.get(emoteID);
-        return emote?emote:emoteName;
+        return emote?emote:"`:"+emoteName+":`";
     }
     function dev(emoteName){
         let emote = client.guilds.get("288029287892910100").emojis.find(e => e.name === emoteName);
-        return emote?emote:emoteName;
+        return emote?emote:"`:"+emoteName+":`";
     }
     function asset(emoteName){
         let emote = client.guilds.get("488443056799088640").emojis.find(e => e.name === emoteName);
-        return emote?emote:emoteName;
+        return emote?emote:"`:"+emoteName+":`";
     }
     return {
         find: find,
