1a84b0c6347cfa66c36853baa7bd8f4923e2eebe
Paweł <zzneix@gmail.com>
2019-08-26 02:50:42 +0200

    updated util

--- utils/timeFormatter.js
+++ utils/timeFormatter.js
@@ -1,11 +1,19 @@
-exports.leadingZeroes = function(n){
+let leadingZeroes = function(n){
     if (n<=9) return "0"+n;
     return n;
 }
 
-function leadingZeroes(n){
-    if (n<=9) return "0"+n;
-    return n;
+exports.leadingZeroes = leadingZeroes;
+
+exports.leadHex = function(n){
+    switch(n.length){
+        case 1:return "00000"+n;
+        case 2:return "0000"+n;
+        case 3:return "000"+n;
+        case 4:return "00"+n;
+        case 5:return "0"+n;
+        case 6:return n;
+    }
 }
 
 exports.msFormat = function(n){
