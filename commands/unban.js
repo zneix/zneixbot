fdb972a53fa36944e5edc2591d9aa9b5154825a3
Paweł <zzneix@gmail.com>
2019-08-27 23:56:58 +0200

    added unban.js

---
+++ commands/unban.js
@@ -0,0 +1,33 @@
+exports.name = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`;
+exports.description = `Unbans previously banned member.`;
+exports.usage = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)} [userID]`;
+exports.perms = ['BAN_MEMBERS'];
+
+exports.run = async (client, message) => {
+    message.cmd = this;
+    message.command(1, async () => {
+        if (!message.guild.me.hasPermission('BAN_MEMBERS')) throw "I don't have **BAN_MEMBERS** permission!\nContact moderators.";
+        if (!/^\d+$/.test(message.args[0])) throw "That is not a user ID!";
+        return execute(message.args[0]);
+
+        async function execute(member){
+            let ban = await message.guild.fetchBan(member);
+            if (!ban) throw "This user is not banned!";
+
+            await message.guild.unban(ban.user);
+            let embed = {
+                color: 0xcae2cd,
+                timestamp: Date.now(),
+                footer: {
+                    text: message.author.tag,
+                    icon_url: message.author.avatarURL
+                },
+                author: {
+                    name: "Successfully Unbanned"
+                },
+                description: `${ban.user} ${ban.reason?`, with previous ban reason: \`${ban.reason}\``:"; there was no previous ban reason."}`
+            };
+            return message.channel.send({embed:embed});
+        }
+    });
+}
