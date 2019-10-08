a50636b326ef0693dbb3b2d28f2fc28c17955c7b
Paweł <zzneix@gmail.com>
2019-10-08 23:04:08 +0200

    added math.js command

---
+++ commands/math.js
@@ -0,0 +1,32 @@
+exports.name = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`;
+exports.description = `Calculates your expression.`;
+exports.usage = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`;
+exports.perms = 'user';
+const math = require('mathjs');
+
+exports.run = (client, message) => {
+    message.cmd = this;
+    message.command(1, async () => {
+        let embed = {
+            color: message.member.displayColor||null,
+            timestamp: message.createdAt,
+            footer: {
+                text: message.author.tag,
+                icon_url: message.author.avatarURL
+            },
+            author: {
+                name: args.join(' ')+' ='
+            },
+            description: thonk().toString()
+        }
+        message.channel.send({embed:embed});
+        function thonk(){
+            try {
+                return math.evaluate(message.args.join(' '));
+            }
+            catch (err){
+                return err;
+            }
+        }
+    });
+}
