cfc93f9271bf6792ca01ba4facffd80054684004
Paweł <zzneix@gmail.com>
2019-10-14 17:51:16 +0200

    addemote supports URLs

--- commands/addemote.js
+++ commands/addemote.js
@@ -1,5 +1,5 @@
 exports.name = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)}`;
-exports.description = `Adds new emote to the current server (currently supports custom emotes only, not links ~~yet~~).`;
+exports.description = `Adds new emote to the current server NOW SUPPORTS LINKS!`;
 exports.usage = `{PREFIX}${__filename.split(/[\\/]/).pop().slice(0,-3)} (emote)`;
 exports.perms = ['MANAGE_EMOJIS'];
 
@@ -7,12 +7,31 @@ exports.run = (client, message) => {
     message.cmd = this;
     message.command(1, async () => {
         if (!message.guild.me.hasPermission('MANAGE_EMOJIS')) throw "I don't have **MANAGE_EMOJIS** permission!\nContact moderators.";
-        if (!/^<:[a-z0-9-_]+:\d+>/i.test(message.args[0]) && !/^<a:[a-z0-9-_]+:\d+>/i.test(message.args[0])) throw "That's not an emote!";
+        let url;
+        let emotename;
+        //resolving discord emotes (from nitro users)
+        if (/^<:[a-z0-9-_]+:\d+>/i.test(message.args[0]) || /^<a:[a-z0-9-_]+:\d+>/i.test(message.args[0])) {
+            let id = /:\d+>/g.exec(message.args[0])[0].slice(1, -1);
+            url = `https://cdn.discordapp.com/emojis/${id}.${/^<a:[a-z0-9-_]+:\d+>/i.test(message.args[0])?"gif":"png"}`;
+            emotename = /^<[a]?:([a-z0-9-_]+):\d+>/gi.exec(message.args[0])[1];
+        }
+        //resolving URLs
+        else {
+            async function urlheaders(){
+                let f = await client.fetch(message.args[0]);
+                return f.headers;
+            }
+            let headers = await urlheaders();
+            if (headers.get('content-type').startsWith('image')){
+                if (headers.get('content-length') > 262143) throw `The requested image is too big! (${Math.floor(headers.get('content-length') / 1024)}kb)! Must be less than 256kb.`;
+                url = message.args[0];
+                emotename = (headers.get('content-location'))?(headers.get('content-location').split('.')[0]):`emoji_${message.guild.emojis.size+1}`;
+            }
+            else throw "That's not an emote nor valid image link!";
+        }
         //got an emote, resolving extension
-        let id = /:\d+>/g.exec(message.args[0])[0].slice(1, -1);
-        let url = `https://cdn.discordapp.com/emojis/${id}.${/^<a:[a-z0-9-_]+:\d+>/i.test(message.args[0])?"gif":"png"}`;
         let m = await message.channel.send('Creating emote...');
-        await message.guild.createEmoji(url, /^<[a]?:([a-z0-9-_]+):\d+>/gi.exec(message.args[0])[1]).catch(err => console.error(err))
+        await message.guild.createEmoji(url, emotename).catch(err => console.error(err))
         .then(async emote => {
             let psa = `Successfully created emote \`${emote.name}\` ${emote}\nReact with ❌ within 15 seconds to remove it or change it's name with typing \`emote <new_name>\``;
             await !m?message.channel.send(psa):m.edit(psa)
