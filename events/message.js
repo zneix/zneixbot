9403dd39f9d76a61c8b8f5ada7d66fab71b21d6e
Paweł <44851575+zneix@users.noreply.github.com>
2019-12-14 23:38:16 +0100

    GAMEBREAKER CONFIG DEFINITION CHANGE
    tl;dr trim() is godlike here...
    -getting guild config from database
    -handling custom prefix per guild there
    -turned prefix() and command() into regular variables
    -no more spaced-prefixes (it is possible to call bot with either 'zb help' or 'zbhelp' because of trim() function that removes every spaces from beginning and end of given string

--- events/message.js
+++ events/message.js
@@ -1,26 +1,24 @@
 module.exports = (client, message) => {
+    if (message.isMemberMentioned(message.guild.me)) message.react(client.config.emojis.peepoPinged); //funny thing to react on mention
     if (message.author.bot || message.channel.type === "dm") return;
-    if (message.isMemberMentioned(message.guild.me)) message.react(client.config.emojis.peepoPinged);
     if (message.mentions.everyone) message.reply("you don't ping everyone "+client.emoteHandler.find("DansGame")); //unfinished, add emote handler
     if (message.content.startsWith(client.user) || message.content.startsWith(`<@!${client.user.id}>`)) message.channel.send(`Hey ${message.author}, my prefix is \`${client.config.prefix}\``, {embed:{color:Math.floor(Math.random()*16777215),description:'[Support Server](https://discordapp.com/invite/cF555AV)'}});
     try {
-        //funny thing to react on mention
-        let prefix = function(){return message.content.substr(0, client.config.prefix.length).toLowerCase();}
-        if (prefix() == client.config.prefix){
+        //getting guild settings
+        message.guild.dbconfig = (await client.db.utils.find('guilds', {guildid: message.guild.id}))[0];
+        if (!message.guild.dbconfig) message.guild.dbconfig = (await client.db.utils.newGuildConfig(message.guild.id)).ops[0];
+        let prefix = message.guild.dbconfig.customprefix===null?client.config.prefix:message.guild.dbconfig.customprefix;
+        if (message.content.substr(0, prefix.length).toLowerCase() === prefix){
             message.perms = require('../utils/permsHandler')(client, message);
             message.perms.isBanned(); //ban check
-            //function for getting command name
-            function command(){
-                if (prefix().endsWith(" ")) return message.content.split(/\s+/gm)[1].toLowerCase();
-                return message.content.split(/\s+/gm).shift(1).slice(prefix().length).toLowerCase();
-            }
-            if (!command()) return; //quick escape in weird cases (e.g. someone types only prefix, no command)
+            //getting command name
+            let command = message.content.slice(prefix.length).trim().split(/\s+/gm)[0].toLowerCase();
+            if (!command) return; //quick escape in weird cases (e.g. someone types only prefix, no command)
             //args declaration
-            if (prefix().endsWith(" ")) message.args = message.content.split(/[ \s]+/gm).slice(2);
-            else message.args = message.content.slice(client.config.prefix.length).split(/[ \s]+/gm).slice(1);
+            message.args = message.content.slice(guildprefix.length).trim().split(/[ \s]+/gm).slice(1);
             //command handling
-            let cmd = client.commands.get(command());
-            if (!cmd) throw `"${command()}" is not a command!`;
+            let cmd = client.commands.get(command);
+            if (!cmd) throw `"${command}" is not a command!`;
             //permission handler
             message.perms.isAllowed(cmd, false);
             //actual running a command
