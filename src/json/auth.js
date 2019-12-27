1ba42841b59eebc2b7f2c251d116d55ab0f35786
Paweł <44851575+zneix@users.noreply.github.com>
2019-12-27 20:26:09 +0100

    got rid of that auth.js

--- src/json/auth.js
+++
@@ -1,9 +0,0 @@
-let obj = {
-    token: process.env.token,
-    db: {
-        host: process.env.dbhost,
-        user: process.env.dbuser,
-        pass: process.env.dbpass
-    }
-}
-module.exports = obj
