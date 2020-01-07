041fddae0784da929b19fea5d57e3bed391825dd
Paweł <44851575+zneix@users.noreply.github.com>
2020-01-08 00:47:39 +0100

    quick reference fix

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,4 +1,4 @@
-module.exports = (client, guild) => {
+module.exports = async (client, guild) => {
     let dbconfig = (await client.db.utils.find('guilds', {guildid: message.guild.id}))[0];
     if (!dbconfig) await client.db.utils.newGuildConfig(message.guild.id);
     require('../src/embeds/guildCreatedDeleted')(guild, client.channels.get(client.config.channels.guildlogs), true);
