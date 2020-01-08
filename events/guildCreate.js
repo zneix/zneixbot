ba7d7577c40778b40f15d681d4c248ca6a265dbd
Paweł <44851575+zneix@users.noreply.github.com>
2020-01-08 20:12:46 +0100

    guild id fix

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,6 +1,6 @@
 module.exports = async (client, guild) => {
-    let dbconfig = (await client.db.utils.find('guilds', {guildid: message.guild.id}))[0];
-    if (!dbconfig) await client.db.utils.newGuildConfig(message.guild.id);
+    let dbconfig = (await client.db.utils.find('guilds', {guildid: guild.id}))[0];
+    if (!dbconfig) await client.db.utils.newGuildConfig(guild.id);
     require('../src/embeds/guildCreatedDeleted')(guild, client.channels.get(client.config.channels.guildlogs), true);
     require('../utils/clientPresence')(client);
 }
