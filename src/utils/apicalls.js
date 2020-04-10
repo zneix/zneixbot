78770da9256f5b966a3c3bb47e734ec22cbbd7d6
zneix <zzneix@gmail.com>
2020-04-11 00:00:00 +0200

    rewrote user command

--- src/utils/apicalls.js
+++ src/utils/apicalls.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch');
 const baseDiscord = 'https://discordapp.com/api';
 const agent = 'zneixbot Project: https://github.com/zneix/zneixbot';
+const formatter = require('./formatter');
 exports.getDiscordUser = async function(client, id){
     //custom query getting partial user profile, used by commands like user.js, avatar.js, etc...
     let response = await fetch(`${baseDiscord}/users/${id}`, {
@@ -11,5 +12,7 @@ exports.getDiscordUser = async function(client, id){
     //hard fix for non-bot users
     if (!response.bot) response.bot = false;
     response.tag = `${response.username}#${response.discriminator}`;
+    response.createdAt = new Date(formatter.snowflake(response.id).timestamp);
+    response.createdTimestamp = formatter.snowflake(response.id).timestamp;
     return response;
 }
