49d4c4140ff0f5844b4452e82bc997c94ef33b2d
zneix <zzneix@gmail.com>
2020-04-10 14:41:48 +0200

    re-added scramble command

---
+++ commands/scramble.js
@@ -0,0 +1,28 @@
+exports.description = 'Generates a scramble for standard rubik\'s cube (3x3x3).';
+exports.usage = '';
+exports.level = 0;
+exports.perms = [];
+exports.cooldown = 3500;
+exports.pipeable = false;
+
+exports.run = async function xd(client, message) {
+    let mainMoves = ['U', 'F', 'R', 'D', 'B', 'L'];
+    let oneOfSix = function(){ return mainMoves[Math.floor(Math.random()*6)];}
+    let random = function(){
+        switch(Math.floor(Math.random()*3)){
+            case 0:
+                return oneOfSix();
+            case 1:
+                return `${oneOfSix()}'`;
+            case 2:
+                return `${oneOfSix()}2`;
+        }
+    }
+    let arr = [];
+    do {
+        let ver = random();
+        if (arr.length) if (arr[arr.length-1].startsWith(ver.slice(0, 1))) continue;
+        arr.push(ver);
+    } while(arr.length < 20);
+    message.channel.send(arr.join(' '));
+}
