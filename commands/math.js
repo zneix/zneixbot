bd15e957fe8d9156655af798d73cad5d0dc8a642
zneix <zzneix@gmail.com>
2020-04-12 04:12:24 +0200

    refactored math command

--- commands/math.js
+++ commands/math.js
@@ -6,26 +6,18 @@ exports.cooldown = 5000;
 exports.pipeable = false;
 
 exports.run = async message => {
-    const math = require('mathjs');
-    let embed = {
-        color: message.member.displayColor||null,
-        timestamp: message.createdAt,
-        footer: {
-            text: message.author.tag,
-            icon_url: message.author.avatarURL
-        },
-        author: {
-            name: message.args.join(' ')+' ='
-        },
-        description: thonk().toString()
+    const fetch = require('node-fetch');
+    let edgeCases = {
+        'π': 'pi',
+        '¹': '^1',
+        '²': '^2',
+        '³': '^3',
+        '¼': '.25',
+        '½': '.5',
+        '¾': '.75'
     }
-    message.channel.send({embed:embed});
-    function thonk(){
-        try {
-            return math.evaluate(message.args.join(' '));
-        }
-        catch (err){
-            return err;
-        }
-    }
+    let expression = message.args.join(' ');
+    Object.keys(edgeCases).forEach(edge => expression = expression.replace(RegExp(edge, 'g'), edgeCases[edge]));
+    let resp = await fetch(`https://api.ivr.fi/math?expr=${expression.split('').map(c => `%${c.charCodeAt(0).toString(16)}`).join('')}`).then(r => r.json());
+    message.channel.send(`\`${expression}\` = ${resp.response}`);
 }
