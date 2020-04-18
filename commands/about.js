4694b5b46a49e4bafb7fe227cf961cdbc5a7a261
zneix <zzneix@gmail.com>
2020-04-18 02:11:54 +0200

    added suggestion and error count

--- commands/about.js
+++ commands/about.js
@@ -2,7 +2,7 @@ exports.description = 'Displays some basic informations about the bot.';
 exports.usage = '';
 exports.level = 0;
 exports.perms = [];
-exports.cooldown = 5000;
+exports.cooldown = 7500;
 exports.dmable = true;
 
 exports.run = async message => {
@@ -15,6 +15,10 @@ exports.run = async message => {
     let runningCommit = shell.execSync('git show --format="%h %at" --summary -q').toString().trim().split(/\s/g); //commit hash, commit timestamp
     let runningNumber = (await fetch(`https://api.github.com/repos/zneix/zneixbot/compare/a0cd321...${runningCommit[0]}`).then(d => d.json())).total_commits;
     let branchInfo = shell.execSync('git status').toString().trim();
+
+    //bot database stats
+    const suggestionCount = await client.db.db().collection('suggestions').countDocuments();
+    const errorCount = await client.db.db().collection('errors').countDocuments();
     //formatting it and editing
     let embed = {
         color: 0xf97304,
@@ -27,6 +31,8 @@ exports.run = async message => {
 Running commit: **#${runningNumber+1} [${runningCommit[0]}](https://github.com/zneix/zneixbot/commit/${runningCommit[0]})${Boolean(branchInfo.match(/Your branch is up to date/)) ? ' [Latest]' : ''}** \`${msToHuman(message.createdAt - parseInt(runningCommit[1])*1000, 3)} ago\`
 Code branch: **${branchInfo.match(/On branch (.+)/)[1]}**
 Commands loaded: **${client.commands.size}**
+Suggestions saved: **${suggestionCount}**
+Errors caught: **${errorCount}**
 Developer: **${client.users.cache.get('288028423031357441').tag}**
 OS: **${process.platform}**
 **✨ [Invite me to your server!](https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)
